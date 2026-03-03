0921d7778f58d1cca50ce922d0731b2e0341e84b
kbd05 <kbd05@Darshan>
2026-03-03 16:07:18 +0530

    first commit

---
+++ CORE_JAVA_01/src/com/tnsif/basics/SecongProgram.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.tnsif.basics;
+
+/**
+ * 
+ */
+public class SecongProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
